78cfea055742fd3e00df1229c9070771dcbfd01e
Gxyloj <gxyloj@gmail.com>
2022-01-07 09:45:27 +0800

    编辑完成 lazy load解决卡顿 子节点无法热刷新

--- src/views/Category/childComps/CategoryList.vue
+++ src/views/Category/childComps/CategoryList.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <el-table  border
-               :data="cateList"
-               row-key=""
+               :data="tableData"
+               row-key="cat_id"
+               lazy
+               :load="load"
+               ref="table"
                >
-<!--      TODO:展开列表极卡 会渲染出大量不显示的二三级菜单-->
+<!--      DONE:展开列表极卡 会渲染出大量不显示的二三级菜单-->
+<!--      TODO:理解load-->
 <!--      <el-table-column type="index" label="#"></el-table-column>-->
       <el-table-column prop="cat_name" label="分类名称"></el-table-column>
       <el-table-column prop="" label="是否有效" width="80px">
@@ -41,7 +45,7 @@
 
   <EditCategoryDialog ref="EditCategoryDialogRef"
                       :editCategoryForm1="editCategoryForm"
-                      @updateList="getCategoryList"/>
+                      @updateList="updateList"/>
 </template>
 
 <script>
@@ -53,14 +57,16 @@ export default {
   components: {EditCategoryDialog},
   data() {
     return {
-      cateList: [],
+      tableData: [],
+      tableDataCopy:[],
       queryInfo: {
         type: 3,
         pagenum: 1,
         pagesize: 5
       },
       total:0,
-      editCategoryForm:{}
+      editCategoryForm:{},
+      showTable:''
     }
   },
   created() {
@@ -70,8 +76,19 @@ export default {
     getCategoryList() {
       getCategoryList(this.queryInfo).then(res => {
         if (res.meta.status !== 200) return
-        console.log(res);
-        this.cateList = res.data.result
+        // console.log(res);
+        // this.cateList = res.data.result
+        this.tableDataCopy = res.data.result || [] // 备份的全量数据
+        this.tableData = JSON.parse(JSON.stringify(res.data.result)).map(item => { // 展示数据
+          // hasChildren 表示需要展示一个箭头图标
+          item.hasChildren = item.children && item.children.length > 0
+          // 只展示一层
+          // 如果有children数据，会自动加载，就不是懒加载了，也可以配置tree-props里面的children为其他字段
+          item.children = null
+          // 记住层级关系
+          item.idList = [item.cat_id]
+          return item
+        })
         this.total = res.data.total
         // console.log(this.cateList);
       })
@@ -93,6 +110,55 @@ export default {
       this.getCategoryList()
 
     },
+    load (tree, treeNode, resolve) {
+      // 层级关系备份
+      const idCopy = JSON.parse(JSON.stringify(tree.idList))
+      // // 查找下一层数据
+      let resolveArr = this.tableDataCopy
+
+      let id
+      // eslint-disable-next-line
+      while (id = tree.idList.shift()) {
+        const tarItem = resolveArr.find(item => item.cat_id === id)
+        tarItem.loadedChildren = true
+        console.log(tarItem);
+        resolveArr = tarItem.children
+      }
+
+      // 处理下一层数据的属性
+      resolveArr = JSON.parse(JSON.stringify(resolveArr))
+      resolveArr.forEach(item => {
+        item.hasChildren = item.children && item.children.length > 0
+        item.children = null
+        // 此处需要深拷贝，以防各个item的idList混乱
+        item.idList = JSON.parse(JSON.stringify(idCopy))
+        item.idList.push(item.cat_id)
+      })
+
+      // 标识已经加载子节点
+      tree.loadedChildren = true
+
+      // 渲染子节点
+      resolve(resolveArr)
+    },
+    unload () {
+      this.showTable = false
+      // eslint-disable-next-line
+      this.$nextTick(() => this.showTable = true)
+      this.tableData = JSON.parse(JSON.stringify(this.tableDataCopy)).map(item => {
+        // hasChildren 表示需要展示一个箭头图标
+        item.hasChildren = item.children && item.children.length > 0
+        // 只展示一层
+        item.children = null
+        // 记住层级关系
+        item.idList = [item.id]
+        return item
+      })
+    },
+    updateList(){
+      this.getCategoryList()
+      this.load()
+    }
   }
 }
 </script>
